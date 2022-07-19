51801c71fb8feeca97a0fbd41abef9feeba99e31
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-07-19 22:47:01 +0200

    submodule--helper: fix "errmsg_str" memory leak
    Fix a memory leak introduced in e83e3333b57 (submodule: port submodule
    subcommand 'summary' from shell to C, 2020-08-13), we sometimes append
    to the "errmsg", and need to free the "struct strbuf".
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -1052,6 +1052,7 @@ static void generate_submodule_summary(struct summary_cb *info,
 	free(displaypath);
 	free(src_abbrev);
 	free(dst_abbrev);
+	strbuf_release(&errmsg);
 }
 
 static void prepare_submodule_summary(struct summary_cb *info,
